260bff9a6980e817857a577a7f826afaf4efabbb
sac083 <sachinktgowda083@gmail.com>
2024-03-20 23:26:12 -0700

    Crud app with mongo and intelliJ

---
+++ mongo-crud-sachin/src/main/java/com/inspiron/mongocrudsachin/controller/NotificationController.java
@@ -0,0 +1,10 @@
+package com.inspiron.mongocrudsachin.controller;
+
+import com.inspiron.mongocrudsachin.response.SuccessResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/notification")
+public class NotificationController {
+    public ResponseEntity<SuccessResponse<>>
+}
